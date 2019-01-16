337e486d70c6a967bba07ff7b3ed2ff4ceab5823
DewHunt <dew.fog1553@gmail.com>
2019-01-16 18:25:14 +0600

    Update - 190116

--- db/mediasource.sql
+++ db/mediasource.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 14, 2019 at 11:45 AM
+-- Generation Time: Jan 16, 2019 at 01:22 PM
 -- Server version: 10.1.33-MariaDB
 -- PHP Version: 7.2.6
 
@@ -71,7 +71,9 @@ INSERT INTO `media` (`Id`, `Name`, `Image`, `EntryBy`, `EntryDateTime`, `UpdateB
 (19, 'Bangladesh Pratidin', 'images/media_logo/bangladesh_pratidin_19011430.jpg', 1, '2019-01-14 10:37:30', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
 (20, 'Jugantor', 'images/media_logo/jugantor_19011401.jpg', 1, '2019-01-14 10:38:01', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
 (21, 'Kaler Kantho', 'images/media_logo/kaler_kantho_19011420.png', 1, '2019-01-14 10:38:20', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
-(22, 'Prothom Alo', 'images/media_logo/prothom_alo_19011435.jpg', 1, '2019-01-14 10:38:35', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00');
+(22, 'Prothom Alo', 'images/media_logo/prothom_alo_19011435.jpg', 1, '2019-01-14 10:38:35', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
+(23, 'Dew Hunt', 'images/media_logo/dew_hunt_19011634.jpg', 1, '2019-01-16 10:56:34', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00'),
+(24, 'Janakantha', 'images/media_logo/janakantha_19011642.jpg', 1, '2019-01-16 13:06:42', 0, '0000-00-00 00:00:00', 0, '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -103,7 +105,7 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `media`
 --
 ALTER TABLE `media`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
